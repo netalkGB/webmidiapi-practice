6bf6a49e7fc27e53edc03eeed032050ed1f327e7
netalkGB <ichikawaorkouki@gmail.com>
2018-12-04 00:44:57 +0900

    忘れてた

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ class Channel {
     this.msb = 0
     this.lsb = 0
     this.program = 0
-    this.isDrum = false
+    this.isDrum = channel === 10
   }
   setBankMSB (param) {
     this.msb = param
